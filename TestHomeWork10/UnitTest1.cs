d762422d12959b7304e87a8477667d1cc4df0e42
Maksym Semenov <mestcomua@gmail.com>
2022-07-23 00:10:38 +0300

    update

---
+++ TestHomeWork10/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Point = HomeWork10.Point;
+
+namespace TestHomeWork10
+{
+    [TestClass]
+    public class Triangle
+    {
+        [TestMethod]
+        public void Perimeter(Point point1, Point point2, Point point3)
+        {
+            //arrenge
+           
+            double expected = 12.488851938354607;
+            //return expected; //5,303300858899108
+
+            //actual
+            Point a = new Point(5, 2);
+            Point b = new Point(2, 5);
+            Point c = new Point(6, 6);
+            Triangle triangle = new Triangle();
+            triangle.Perimeter(a, b, c);
+             
+
+            //assert
+            Assert.Equals(expected, result);
+        }
+
+    }
+}
